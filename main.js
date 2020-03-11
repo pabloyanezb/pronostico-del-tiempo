c572c6af38bab1513a2408e65d5e011ddb7423b1
Pablo Yanez <pabloyanezbarriga@gmail.com>
2020-03-11 10:44:52 -0300

    Mapa agregado y datos puestos en tabla

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
+function mostrar(ubicacion){
+    console.log(ubicacion);
+}
+navigator.geolocation.getCurrentPosition(mostrar);
+
 $(document).ready(function(){
     $('.btn').on('click',function() {
-        $(this).parent().nextAll().remove();
+        if($('input').val()==''){
+            return;
+        }
+        $('.jumbotron').find('table').children().remove();
+        $('.jumbotron').find('iframe').remove();
         var ciudad = $('input').val();
-        $.get( `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&&appid=67f315290d8c351082f9bf3ddea9ea7f`, function(res) {
+        $.get( `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=es&&appid=67f315290d8c351082f9bf3ddea9ea7f`, function(res) {
             
-            $('.jumbotron').append(`<h3>${ciudad}: ${res.weather[0].description}</h3>`);
-            $('.jumbotron').append(`<h3>Temperatura : ${res.main.temp-273.15}C°</h3>`);
-            $('.jumbotron').append(`<h3>Coordenadas : ${res.coord.lon}, ${res.coord.lat}</h3>`);
+            $('.table').append(
+                `<tr>
+                    <td colspan="3" class="ciudad">${res.name}</td>
+                </tr>`);
+            $('.table').append(
+                `<tr>
+                    <td>Descripción del tiempo:</td>
+                    <td class="descripcion"> ${res.weather[0].description}</td>
+                </tr>`);
+            $('.table').append(
+                `<tr>
+                    <td>Temperatura :</td>
+                    <td> ${Math.round(res.main.temp-273.15)}C°</td>
+                <tr>`);
+            $('.table').append(
+                `<tr>
+                    <td>Coordenadas :</td>
+                    <td> ${res.coord.lon}, ${res.coord.lat}</td>
+                </tr>);`);
+            $('.jumbotron').append(`<iframe class="iframe mapa" src="https://maps.google.com/?ll=${res.coord.lat},${res.coord.lon}&z=14&t=m&output=embed" height="300" width="1085" frameborder="0" style="border:0" allowfullscreen></iframe>`);
             console.log(res);
         }, 'json');
 
